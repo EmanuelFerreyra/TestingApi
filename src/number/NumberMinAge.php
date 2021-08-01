4dc608f4d76d4e6084b612cb4d800f703fc9ee57
Emanuel Ferreyra <ferreyra-emanuel@outlook.com>
2021-08-01 16:52:46 -0300

    Se implemto NumberMinAge , para identificar a usuarios mayores segun el parametro

---
+++ src/number/NumberMinAge.php
@@ -0,0 +1,20 @@
+<?php
+
+class NumberMinAge implements Number{
+    
+    protected $age;
+
+    public function __construct( $age ){ 
+        $this->age = $age;
+    }   
+    
+    public function setNumber( $age ){
+        $this->number = $age;
+    }    
+    
+    public function getNumber(){
+        return $this->age;
+    }
+
+}
+
