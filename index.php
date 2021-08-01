22693954eea9588c9bd52e8b1bac9f5d8e084e1c
Emanuel Ferreyra <ferreyra-emanuel@outlook.com>
2021-08-01 06:26:45 -0300

    se agrego interface Text para tener diferentes implementaciones

--- index.php
+++ index.php
@@ -1,29 +1,90 @@
 <?php
 
-class HelloWord{
 
-    private $name;
-
-    function __construct( $name ){
-        $this->name = $name;
-    }
-
-    public function setName( $name ){
-
-        $this->name = $name;
-        return $this;
-    }
-
-    public function getName(){
-        return $this->name;
-    }
-
+interface Text
+{
+    public function setText($text);
+    public function getText();
 }
 
 
-$hello = new HelloWord('Carlos');
+class HelloWord implements Text
+{
 
-$hello->getName();
+    private $text;
 
-$hello->setName('Carlos Menen')
-        ->getName();
+    function __construct($text)
+    {
+        $this->text = $text;
+    }
+
+    public function setText($text)
+    {
+
+        $this->text = $text;
+        return $this;
+    }
+
+    public function getText()
+    {
+        return $this->text;
+    }
+}
+
+class HelloAmerica implements Text
+{
+
+    private $text;
+
+    function __construct()
+    {
+        $this->text = 'Hola America';
+    }
+
+    public function setText($text)
+    {
+
+        $this->text = $text;
+        return $this;
+    }
+
+    public function getText()
+    {
+        return $this->text;
+    }
+}
+
+class UsertHtml
+{
+
+    public $txt;
+
+    public function __construct(Text $txt)
+    {
+        $this->txt = $txt;
+    }
+
+
+    public function onTxt()
+    {
+        return $this->txt;
+    }
+}
+
+
+//Test
+$view = new UsertHtml(new HelloAmerica);
+print $view->onTxt()
+    ->getText();
+print '------------';
+print $view->onTxt()
+    ->setText('Hola America , como estan?')
+    ->getText();
+
+$viewWord = new UsertHtml(new HelloWord('Hola mundo'));
+print $viewWord->onTxt()
+    ->getText();
+print '------------';
+print $viewWord->onTxt()
+    ->setText('Hola Mundo, como estan?')
+    ->getText();
