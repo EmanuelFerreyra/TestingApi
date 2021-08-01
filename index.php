1908cd8ed2daabd96dc5d4d2cbab39e85893f81f
Emanuel Ferreyra <ferreyra-emanuel@outlook.com>
2021-07-31 22:19:52 -0300

    se agrego clase helloword para una mejor reutilizacion

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
 
-print 'Hello Word';
+class HelloWord{
+
+    private $name;
+
+    function __construct( $name ){
+        $this->name = $name;
+    }
+
+    public function setName( $name ){
+
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+}
+
+
+$hello = new HelloWord('Carlos');
+
+$hello->getName();
+
+$hello->setName('Carlos Menen')
+        ->getName();
